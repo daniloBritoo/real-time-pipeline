7f656d5469eb0ebafa338171c66465aad106e3e6
daniloBritoo <danilo.britto70@gmail.com>
2025-07-23 22:14:29 -0300

    fix/adjust_producer_serializer

--- producer/producer.py
+++ producer/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 import json, time, random
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 while True:
     data = {
@@ -11,3 +11,4 @@ while True:
     print(f"enviando: {data}")
     producer.send("temperature_sensor", value=data)
     time.sleep(1)
+
